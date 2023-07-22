d17f69d67757b952f6320de3bdf63e22be3faa4e
chal2and <63391804+chal2andev@users.noreply.github.com>
2023-07-22 23:01:54 +0900

    버튼추가

---
+++ Assets/UI/LeftButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftButton : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
